26466343aab243a517ae13d49b587bb6a5841510
Tianrun Gu <tianrungu@Tianruns-MacBook-Pro.local>
2020-03-18 14:57:17 -0400

    finish log in window

---
+++ src/LoginController.java
@@ -0,0 +1,32 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private Label passLabel;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void verifyUser(ActionEvent event) {
+
+    }
+
+}
